f55ba7aeb22789e8ddf8d6ddc33fceca67b8e3ad
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-01-31 21:17:54 +0300

    added hooks

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -1,5 +1,5 @@
-import React from "react";
-// This component will be consuming our context 
+import React, { useContext } from "react";
+// This component will be consuming our context
 // so we need to import it in order to use it
 import { MyContext } from "./context/MyProvider";
 
@@ -17,25 +17,41 @@ import { MyContext } from "./context/MyProvider";
 //   );
 // };
 
-
 // This component will be wrapped in our context.
 // Consumer so we have access to the global state and methods declared there.
 // It only accepts a child a function that will return some html,
 // this function has as an argument the value of our consumer (which stores the state and our methods)
 
-const Section5 = () => (
-  <MyContext.Consumer>
-    {(value) => (
-      <div>
-        SECTION 5 <br />
-        Name: {value.state.name}
-        <br />
-        Age: {value.state.age}
-        <br />
-        <button onClick={value.addAction}>+</button>
-        <button onClick={value.subtractAction}>-</button>
-      </div>
-    )}
-  </MyContext.Consumer>
-);
+// const Section5 = () => (
+//   <MyContext.Consumer>
+//     {(value) => (
+//   <div>
+//     SECTION 5 <br />
+//     Name: {value.state.name}
+//     <br />
+//     Age: {value.state.age}
+//     <br />
+//     <button onClick={value.addAction}>+</button>
+//     <button onClick={value.subtractAction}>-</button>
+//   </div>
+//     )}
+//   </MyContext.Consumer>
+// );
+
+const Section5 = () => {
+  const context = useContext(MyContext);
+
+  return (
+    <div>
+      SECTION 5 <br />
+      Name: {context.state.name}
+      <br />
+      Age: {context.state.age}
+      <br />
+      <button onClick={context.addAction}>+</button>
+      <button onClick={context.subtractAction}>-</button>
+    </div>
+  );
+};
+
 export default Section5;
--- src/components/context/MyProvider.js
+++ src/components/context/MyProvider.js
@@ -1,42 +1,61 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 
 // It creates our context object, later we will use it to define our Provider or Consumer
 // We need to export it because when we use the Consumer, it will be in a different file
-export const MyContext = React.createContext()
+export const MyContext = React.createContext();
 
-
-// We create a class component that will store 
+// We create a class component that will store
 // the global state of the app and the methods we use
-class MyProvider extends Component {
-  state = {
-    name: "TOMMY",
-    age: 33,
-  };
+// class MyProvider extends Component {
+//   state = {
+//     name: "TOMMY",
+//     age: 33,
+//   };
 
-  addAction = () => {
-    this.setState({
-      age: this.state.age + 1,
-    });
-  };
+//   addAction = () => {
+//     this.setState({
+//       age: this.state.age + 1,
+//     });
+//   };
 
-  subtractAction = () => {
-    this.setState({
-      age: this.state.age - 1,
-    });
-  };
-  render() {
-    return (
-      <MyContext.Provider
-        value={{
-          state: this.state,
-          addAction: this.addAction,
-          subtractAction: this.subtractAction,
-        }}
-      >
-        {this.props.children}
-      </MyContext.Provider>
-    );
-  }
-}
+//   subtractAction = () => {
+//     this.setState({
+//       age: this.state.age - 1,
+//     });
+//   };
+//   render() {
+
+const initialState = {
+  name: "CHIKA",
+  age: 18,
+};
+
+const MyProvider = (props) => {
+  const [state, setState] = useState(initialState);
+
+  const addAction = () => setState({
+    name: 'SUPER-CHIKA',
+    age: state.age + 1,
+  });
+
+  const subtractAction = () => setState({
+    name: 'CHIKKKA',
+    age: state.age - 1,
+  });
+
+  return (
+    <MyContext.Provider
+      value={{
+        state: state,
+        addAction: addAction,
+        subtractAction: subtractAction,
+      }}
+    >
+      {props.children}
+    </MyContext.Provider>
+  );
+  //}
+};
 
 export default MyProvider;
